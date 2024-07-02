00c02824acdc3547adedab9fd72c48f3e6cf16a5
mmingoo <aldrn0323@naver.com>
2024-07-02 10:37:57 +0900

    second bfs

--- BFS/note.py
+++ BFS/note.py
@@ -1,21 +1,19 @@
-#0628
-#0702
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+cnt = 0
 maxv = 0
-count = 0
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-def bfs(y, x):
+def bfs(y,x):
     rs = 1
-    q = deque([(y, x)])
+    q = deque([(y,x)])
     visited[y][x] = True
 
     while q:
@@ -25,19 +23,18 @@ def bfs(y, x):
             ny = ey + dy[k]
             nx = ex + dx[k]
 
-            if 0 <= ny < n and 0 <= nx < m:
+            if 0<= ny < n and 0 <= nx < m:
                 if graph[ny][nx] == 1 and not visited[ny][nx]:
-                    rs += 1
+                    rs+=1
+                    q.append((ny,nx))
                     visited[ny][nx] = True
-                    q.append((ny, nx))
-
     return rs
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1 and not visited[i][j]:
-            maxv = max(maxv, bfs(i, j))
-            count += 1
+            cnt += 1
+            maxv = max(maxv, bfs(i,j))
 
-print(count)
+print(cnt)
 print(maxv)
