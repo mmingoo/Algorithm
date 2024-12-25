0534669797ac98c02c80f3d3ba9cc05b3e7f3d22
mmingoo <aldrn0323@naver.com>
2024-12-25 23:18:59 +0900

    4889 복습

--- stack/4889-s1.py
+++ stack/4889-s1.py
@@ -1,22 +1,28 @@
-answer = []
+import sys
+input = sys.stdin.readline
+rank = 0
 
 while True:
     stack = []
-    count = 0
-    s = input()
+    cnt = 0
+    s = list(map(str,input().rstrip()))
+    rank += 1
+
     if '-' in s:
         break
-    for i in range(len(s)):
-        if not stack and s[i] == '}':
-            count += 1
-            stack.append('{')
-        elif stack and s[i] == '}':
-            stack.pop()
-        else:
-            stack.append(s[i])
-    count += len(stack)//2
-    answer.append(count)
 
-for i in range(len(answer)):
-    print(i+1, '. ', answer[i], sep='')
+    for ch in s:
+        if ch == "{":
+            stack.append(ch)
+        else:
+            if stack:
+                stack.pop()
+            
+            else:
+                cnt += 1
+                stack.append('{')
+
+    cnt += len(stack)//2
+    print(f"{rank}. {cnt}")
+
 
