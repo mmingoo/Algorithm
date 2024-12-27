761e645632508b5473e84f1492255e63d8d2150a
mmingoo <aldrn0323@naver.com>
2024-12-27 17:34:29 +0900

    17516-s1 실패

--- stack/test.py
+++ stack/test.py
@@ -1,34 +1,39 @@
 import sys
 input = sys.stdin.readline
-stack = []
-ing = True
-rank = 0
+n = int(input().rstrip())
+balls = input().rstrip()
+red = balls.count('R')
+blue = n - red
+res = min(red, blue)
+cont = 1
 
-while ing:
-    cnt = 0
-    rank += 1
-    stack = []
-    str_list = input().rstrip()
-
-    for i in str_list:
-        if i == "}":
-            if stack:
-                stack.pop()
-            else:
-                stack.append("{")
-                cnt+=1
-
-        elif i == "{":
-            stack.append("{")
-
-        elif i == "-":
-            ing = False
-            break
-
-    if ing:
-        cnt += len(stack) // 2
-        print(f"{rank}. {cnt}")
+# 맨 왼쪽에서 연속된 볼의 갯수 카운트
+for i in range(1, n):
+    if balls[i] == balls[i - 1]:
+        cont += 1
+    else:
+        break
+# 왼쪽이 r로 시작되는 경우 계산
+if balls[0] == 'R':
+    res = min(res, red - cont)
+# 왼쪽이 b로 시작되는 경우 계산
+else:
+    res = min(res, blue - cont)
+cont = 1
 
 
+
+# 오른쪽에 모여있는 볼들을 검사하는 것
+for i in range(n - 2, -1, -1):
+    if balls[i] == balls[i + 1]:
+        cont += 1
+    else:
+        break
+
+if balls[-1] == 'R':
+    res = min(res, red - cont)
+else:
+    res = min(res, blue - cont)
+print(res)
 
 
