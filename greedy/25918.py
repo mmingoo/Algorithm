20ceeba9dab007c87923a70ad938067995e3cb90
mmingoo <aldrn0323@naver.com>
2024-12-28 10:52:10 +0900

    25918 복습 성공

--- greedy/25918.py
+++ greedy/25918.py
@@ -1,20 +1,50 @@
 import sys
 
 input = sys.stdin.readline
-N,L = map(int,input().split())
-spot = list(map(int, input().split()))
-spot.sort()
-
+n = int(input())
+str_list = input().strip()
+stack = []
 cnt = 0
-start = 0
-end = 0
-for i in spot:
+max_depth = 0
 
-    if start < i < end:
-        pass
+for ch in str_list:
+    if ch == '(':
+        if len(stack) == 0 or stack[-1] == '(':
+            stack.append(ch)
+            cnt = len(stack)
+            max_depth = max(cnt, max_depth)
+
+        else:
+            stack.pop()
     else:
-        start = i - 0.5
-        end = i + L
-        cnt += 1
+        if len(stack) == 0 or stack[-1] == ')':
+            stack.append(ch)
+            cnt = len(stack)
+            max_depth = max(cnt, max_depth)
+        else:
+            stack.pop()
 
-print(cnt)
+
+if stack:
+    print(-1)
+else:
+    print(max_depth)
+
+
+
+## 두번째
+N = int(input())
+S = input()
+min_day = 0
+total = 0
+for char in S:
+    if char == '(':
+        total += 1
+    else:
+        total -= 1
+    if abs(total) > min_day:
+        min_day = abs(total)
+if total == 0:
+    print(min_day)
+else:
+    print(-1)
