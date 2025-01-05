09e8ef17784bacafa13457dcc68bf49ed06ba8b3
mmingoo <aldrn0323@naver.com>
2025-01-05 23:25:40 +0900

    4889-복습

--- stack/4889-s1.py
+++ stack/4889-s1.py
@@ -26,3 +26,36 @@ while True:
     print(f"{rank}. {cnt}")
 
 
+
+
+# 두번째 풀이 - 맞춤
+import sys, heapq
+
+input = sys.stdin.readline
+stack = []
+rank = 0
+while 1:
+    rank += 1
+    cnt = 0
+    chr_list = list(input().rstrip())
+    stack.clear()
+
+    if chr_list[0] == "-":
+        break
+
+    else:
+        for i in range(len(chr_list)):
+            if chr_list[i] == "}":
+                if stack:
+                    stack.pop()
+                else:
+                    cnt+=1
+                    stack.append("{")
+
+            else:
+                stack.append("{")
+
+    if stack:
+        cnt+=len(stack)//2
+
+    print(f"{rank}. {cnt}")
--- test.py
+++ test.py
@@ -1,28 +1,30 @@
-import sys
-import heapq
+import sys, heapq
+
 input = sys.stdin.readline
-n = int(input())
-heap = []
-target = 0
-cnt = 0
-max_idx = 0
-for _ in range(n):
-    heap.append(int(input()))
-
-
-if n == 1:
+stack = []
+rank = 0
+while 1:
+    rank += 1
     cnt = 0
-else:
-    target = heap[0]
-    del heap[0]
+    chr_list = list(input().rstrip())
+    stack.clear()
 
-    while 1:
-        max_idx = heap.index(max(heap))
-        if target <= heap[max_idx]:
-            target += 1
-            heap[max_idx] -= 1
-            cnt += 1
-        else:
-            break
+    if chr_list[0] == "-":
+        break
 
-print(cnt)
+    else:
+        for i in range(len(chr_list)):
+            if chr_list[i] == "}":
+                if stack:
+                    stack.pop()
+                else:
+                    cnt+=1
+                    stack.append("{")
+
+            else:
+                stack.append("{")
+
+    if stack:
+        cnt+=len(stack)//2
+
+    print(f"{rank}. {cnt}")
