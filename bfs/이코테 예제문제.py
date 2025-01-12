d58f9cb4a08bd85c7baeda9f684e943f5b8d364a
mmingoo <aldrn0323@naver.com>
2025-01-12 15:39:26 +0900

    이코테 bfs 예제문제

--- bfs/이코테 예제문제.py
+++ bfs/이코테 예제문제.py
@@ -1,25 +1,24 @@
 from collections import deque
+
 n, m = map(int, input().split())
 graph = []
 
 for i in range(n):
-    graph.append(list(map(int,input())))  # 문자열로 입력됨
-
+    graph.append(list(map(int, input())))  # 문자열로 입력됨
 
 # 상하좌우
-dy = [0,0,-1,1]
-dx = [-1,1,0,0]
+dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
 
 
-
-def bfs(x,y):
+def bfs(x, y):
     queue = deque()
-    queue.append((x,y))
+    queue.append((x, y))
 
     while deque:
         for i in range(4):
-            ny = y+dy[i]
-            nx = x+dx[i]
+            ny = y + dy[i]
+            nx = x + dx[i]
 
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
@@ -28,7 +27,8 @@ def bfs(x,y):
                 continue
 
             if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y]+1
-    return graph[n-1][m-1]
+                graph[nx][ny] = graph[x][y] + 1
+    return graph[n - 1][m - 1]
 
-print(bfs(0,0))
+
+print(bfs(0, 0))
