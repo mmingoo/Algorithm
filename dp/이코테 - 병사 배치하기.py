f44a26d6f89c43f3eacdd5d4aea0e9421c4b3846
mmingoo <aldrn0323@naver.com>
2025-01-21 14:26:56 +0900

    이코테 병사 배치하기

--- dp/이코테 - 병사 배치하기.py
+++ dp/이코테 - 병사 배치하기.py
@@ -4,9 +4,7 @@ array.reverse()
 
 dp = [1] * n
 
-for i in range(1, n):
+for i in range(1,n):
     for j in range(0,i):
         if array[j] < array[i]:
-            dp[i] += max(dp[i], dp[j] + 1)
-
-print(n-max(dp))
+            dp[i] = max(dp[i], dp[j]+1)
