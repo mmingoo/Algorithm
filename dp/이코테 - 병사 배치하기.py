327b228c5178a7709c8fb76a750d67100fa22bed
mmingoo <aldrn0323@naver.com>
2025-01-21 15:06:42 +0900

    이코테 병사 배치하기

--- dp/이코테 - 병사 배치하기.py
+++ dp/이코테 - 병사 배치하기.py
@@ -1,10 +1,14 @@
 n = int(input())
-array = list(map(int,input().split()))
-array.reverse()
-
-dp = [1] * n
-
-for i in range(1,n):
-    for j in range(0,i):
-        if array[j] < array[i]:
-            dp[i] = max(dp[i], dp[j]+1)
+dp = [-1]* 5001
+dp[3] = 1
+dp[5] = 1
+for i in range(6, n+1):
+    # 이전 값들로부터의 계산을 먼저 시도
+    if dp[i-3] != -1 and dp[i-5] != -1:
+        dp[i] = 1 + min(dp[i-3] , dp[i-5])
+    # 나누어 떨어지는 경우는 나중에 체크
+    elif i % 5 == 0:
+        dp[i] = i // 5
+    elif i % 3 == 0:
+        dp[i] = i // 3
+print(dp[n])
