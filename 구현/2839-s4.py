b81248190050836632aa9fc4c3e51603e2847fd2
mmingoo <aldrn0323@naver.com>
2025-01-21 16:55:06 +0900

    2852- 실패

--- 구현/2839-s4.py
+++ 구현/2839-s4.py
@@ -9,28 +9,24 @@ for i in range(n):
     team, time = input().split()
     m, s = map(int, time.split(':'))
 
-    # 1팀이 득점한 경우
     if team == '1':
-
-        # 득점하기 전에 동점인 경우
         if flag == 0:
-            one = 48 * 60 - (m*60 + s)
-        flag += 1
+            one += 48 * 60 - (60 * m + s)
 
+        flag += 1
         if flag == 0:
             if two > 0:
-                two = two - (48 * 60 - ( m * 60 + s))
+                two = two - (48 * 60 - (60 * m + s))
 
-     # 2팀이 득점한 경우
+
     else:
         if flag == 0:
-            two = 48 * 60 - (m * 60 + s)
+            two += 48 * 60 - (60 * m + s)
 
         flag -= 1
-
-        if one > 0:
-            one = one  -  (48 * 60 - ( m * 60 + s))
-
+        if flag == 0:
+            if one > 0:
+                one = one - (48 * 60 - (60 * m + s))
 
 print('{:0>2}:{:0>2}'.format(one // 60, one % 60))
 print('{:0>2}:{:0>2}'.format(two // 60, two % 60))
