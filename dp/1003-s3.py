a70d19831d4889abbde9794c850e0d1d7fecc6e3
mmingoo <aldrn0323@naver.com>
2025-01-24 15:46:05 +0900

    1003 -s3 실패

--- dp/1003-s3.py
+++ dp/1003-s3.py
@@ -4,5 +4,5 @@ for _ in range(T):
     a, b = 1, 0 # 0과 1이 호출된 횟수
     for i in range(N):
         # 0은 1이 호출된 횟수만큼, 1은 0과 1이 호출된 합만큼 출력됨
-        a,b = b, a+b 
+        a,b = b, a+b
     print(a,b)
