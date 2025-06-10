8f2e2bf2c76603388b25142863373892fda66edf
mmingoo <aldrn0323@naver.com>
2025-06-10 13:26:20 +0900

    프로그래머스 -'모의고사' 두번째 풀이

--- programmers/brute_force/모의고사.py
+++ programmers/brute_force/모의고사.py
@@ -15,3 +15,33 @@ def solution(answers):
     return [i + 1 for i, v in enumerate(s) if v == max(s)]
 
 
+# 내가 푼 두번쨰 방식
+
+def solution(answers):
+    correct = [0] * (3)
+    max_cnt = []
+    correct1 = [1, 2, 3, 4, 5]
+    correct2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    correct3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    for i in range(len(answers)):
+        if answers[i] == correct1[i % 5]:
+            # print(f" i : {i} /i%5 : {i%5}/answers[i] : {answers[i]} / correct1[i%5] : {correct1[i%5]} /  ")
+            correct[0] += 1
+        if answers[i] == correct2[i % 8]:
+            correct[1] += 1
+        if answers[i] == correct3[i % 10]:
+            correct[2] += 1
+
+    max_v = max(correct)
+
+    for i in range(len(correct)):
+        if correct[i] == max_v:
+            max_cnt.append(i + 1)
+
+    return max_cnt
+
+
+
+
+
