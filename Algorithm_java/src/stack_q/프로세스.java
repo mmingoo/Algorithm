4f5bac4425eae5d6bcd10459cde05b4dd1bf68f2
mmingoo <aldrn0323@naver.com>
2025-09-07 18:10:41 +0900

    프로그래머스 - 프로세스 풀이

--- Algorithm_java/src/stack_q/프로세스.java
+++ Algorithm_java/src/stack_q/프로세스.java
@@ -1,112 +1,58 @@
 
-
 import java.util.*;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
-        int cnt = 0;
-        Deque<Task> dq = new ArrayDeque<>();
+    class Truck {
+        int weight;
+        int move;
 
-        // 큐에 작업들 추가
-        for(int i = 0; i<priorities.length; i++){
-            dq.offer(new Task(i, priorities[i]))
+        public Truck(int weight) {
+            this.weight = weight;
+            this.move = 1;
         }
 
-        while(!dq.isEmpty()){
+        public void moving() {
+            move++;
+        }
+    }
 
-            max = dq.stream()
-                    .mapToInt()
+    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+        Queue<Truck> waitQ = new LinkedList<>();
+        Queue<Truck> moveQ = new LinkedList<>();
+
+        for (int t : truckWeights) {
+            waitQ.offer(new Truck(t));
         }
 
-        return cnt;
-    }
-}
+        int answer = 0;
+        int curWeight = 0;
 
-class Task{
-    int index;
-    int pr;
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
 
-    public Task(int index, int pr){
-        this.index = index;
-        this.pr = pr;
-    }
+            if (moveQ.isEmpty()) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
+                continue;
+            }
 
-    public int getIndex(){
-        return this.index;
-    }
+            for (Truck t : moveQ) {
+                t.moving();
+            }
 
-    public int getPr(){
-        return this.pr;
-    }
-}
+            if (moveQ.peek().move > bridgeLength) {
+                Truck t = moveQ.poll();
+                curWeight -= t.weight;
+            }
 
-
-
-
-
-
-
-
-
-
-import java.util.*;
-
-class Solution {
-    public int solution(int[] priorities, int location) {
-        int cnt = 0;
-        Deque<Task> dq = new ArrayDeque<>();
-
-        // 큐에 작업들 추가
-        for(int i = 0; i < priorities.length; i++){
-            Task task = new Task(i, priorities[i]);  // priorities[i] 수정
-            dq.offer(task);
-        }
-
-        while(!dq.isEmpty()) {
-            // 현재 큐에서 최대 우선순위 찾기
-            int max = dq.stream()
-                    .mapToInt(Task::getPr)
-                    .max()
-                    .orElse(0);
-
-            Task current = dq.poll();
-
-            // 현재 작업이 최대 우선순위가 아니면 뒤로 보내기
-            if(current.getPr() < max) {
-                dq.offer(current);
-            } else {
-                // 최대 우선순위면 실행
-                cnt++;
-                if(current.getIndex() == location) {
-                    break;
-                }
+            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
+                Truck t = waitQ.poll();
+                curWeight += t.weight;
+                moveQ.offer(t);
             }
         }
 
-        return cnt;
+        return answer;
     }
 }
-
-class Task{
-    int index;
-    int pr;
-
-    public Task(int index, int pr){
-        this.index = index;
-        this.pr = pr;
-    }
-
-    public int getIndex(){
-        return this.index;
-    }
-
-    public int getPr(){
-        return this.pr;
-    }
-}
-
-
-
-
-
-
